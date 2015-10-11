3cd16eb217882d387e24b3bd3faef68202a7b4e1
emma <emaa.lp@hotmail.com>
2015-10-11 18:00:31 -0300

    home

---
+++ anuri/src/views/home.java
@@ -0,0 +1,43 @@
+package views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class home {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					home window = new home();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public home() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
